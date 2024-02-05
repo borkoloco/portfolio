a68b67a8ed024c86808188cae5a930d99963401f
Santiago Bosca Chillida <borkoloco@MacBook-Air-de-Santiago.local>
2024-02-05 15:51:05 -0300

    portfolio

--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -9,7 +9,7 @@ export default function Skills() {
       <h2 className={`page-header page-header-${theme}`}>Resume</h2>
       <div className="resume__container">
         <a
-          href="https://res.cloudinary.com/ddx1ikpma/image/upload/v1703889203/resume_xwxwn7.png"
+          href="https://res.cloudinary.com/ddx1ikpma/image/upload/v1707158899/resume4_xcc7st.png"
           className="resume__link"
           rel="noreferrer"
           target="_blank"
