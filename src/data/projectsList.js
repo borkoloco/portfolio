83f7127ecab0db4985e4ef88e50220ecf714a464
Santiago Bosca Chillida <borkoloco@MacBook-Air-de-Santiago.local>
2024-02-29 02:28:09 -0300

    holu

--- src/data/projectsList.js
+++ src/data/projectsList.js
@@ -6,8 +6,8 @@ const projects = [
       "Restaurant application where you can use different services such as selecting the menu and booking your seat and pay the whole thing before arriving",
     image:
       "https://res.cloudinary.com/ddx1ikpma/image/upload/v1703647150/Captura_de_pantalla_2023-12-18_a_la_s_13.49.04_moeubt.png",
-    // live: "https://foodexpress-alpha.vercel.app",
-    live: "https://streamable.com/x2ud50",
+    live: "https://foodexpress-alpha.vercel.app",
+    // live: "https://streamable.com/x2ud50",
     github: "https://github.com/borkoloco/FoodExpress-Front",
     builtWith: [
       "SiJavascript",
@@ -26,8 +26,8 @@ const projects = [
       "SPA based on pokemon api where I used techs learned during Henry Bootcamp: React, Redux, Node, Express and Sequelize.",
     image:
       "https://res.cloudinary.com/ddx1ikpma/image/upload/v1703646904/Captura_de_pantalla_2023-12-18_a_la_s_13.46.56_upu43t.png",
-    // live: "https://pokemon-client-5pda.onrender.com",
-    live: "https://streamable.com/g7o82l",
+    live: "https://pokemon-client-5pda.onrender.com",
+    // live: "https://streamable.com/g7o82l",
     github: "https://github.com/borkoloco/PI-Pokemon-main",
     builtWith: ["SiJavascript", "FaReact", "SiJest", "SiExpress", "SiDotenv"],
   },
@@ -38,8 +38,8 @@ const projects = [
       "SPA based on Rick and Morty api where I used techs learned during Henry Bootcamp: React, Redux, Node, Express and Sequelize.",
     image:
       "https://res.cloudinary.com/ddx1ikpma/image/upload/v1703647028/Captura_de_pantalla_2023-12-18_a_la_s_13.44.41_dgoon1.png",
-    // live: "https://integrador2-zo58.onrender.com",
-    live: "https://streamable.com/kew48k",
+    live: "https://integrador2-zo58.onrender.com",
+    // live: "https://streamable.com/kew48k",
     github: "https://github.com/borkoloco/Integrador",
     builtWith: ["SiJavascript", "FaReact", "SiJest", "SiExpress", "SiDotenv"],
   },
@@ -50,8 +50,8 @@ const projects = [
       "Jobs Management App created with Vite, Express, MongoDB, and Styled Components.",
     image:
       "https://res.cloudinary.com/ddx1ikpma/image/upload/v1704250578/Captura_de_pantalla_2024-01-02_a_la_s_23.44.59_gibiki.png",
-    // live: "https://jobify-top6.onrender.com/",
-    live: "https://streamable.com/kv92e7",
+    live: "https://jobify-top6.onrender.com/",
+    // live: "https://streamable.com/kv92e7",
     github: "https://github.com/borkoloco/jobify",
     builtWith: [
       "SiJavascript",
@@ -68,8 +68,8 @@ const projects = [
       "Movie Management App created with Django, Python3, Sqlite, and Bootstrap.",
     image:
       "https://res.cloudinary.com/ddx1ikpma/image/upload/v1708373149/Captura_de_pantalla_2024-02-19_a_la_s_17.02.52_cnstm2.png",
-    // live: "https://borkoloco.pythonanywhere.com/",
-    live: "https://streamable.com/2dgelk",
+    live: "https://borkoloco.pythonanywhere.com/",
+    // live: "https://streamable.com/2dgelk",
     github: "https://github.com/borkoloco/vidly",
     builtWith: ["SiDjango", "SiSqlite", "SiPython", "SiBootstrap"],
   },
